85c5de4abb4ca908dd10d76612f0559a46d0a47f
Jill Cardamon <jillian.cardamon@mapbox.com>
2021-11-07 10:41:57 -0500

    Calculate matrices and finish doc comments in options.

--- Sources/MapboxDirections/Matrix.swift
+++ Sources/MapboxDirections/Matrix.swift
@@ -1,4 +1,172 @@
-
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 // TODO: HANDLE ACTUAL CALL HERE
+
+open class Matrix {
+    
+    /**
+     A tuple type representing the matrix session that was generated from the request.
+     
+     - parameter options: A `MatrixOptions ` object representing the request parameter options.
+     
+     - parameter credentials: A object containing the credentials used to make the request.
+     */
+    public typealias Session = (options: MatrixOptions, credentials: DirectionsCredentials)
+    
+    /**
+     A closure (block) to be called when a matrix request is complete.
+     
+     - parameter session: A `Matrix.Session` object containing session information
+     
+     - parameter result: A `Result` enum that represents the (RETURN TYPE) if the request returned successfully, or the error if it did not.
+     */
+    // TODO: GET RETURN TYPE SORTED OUT AND UPDATE THIS COMPLETION HANDLER
+    public typealias MatrixCompletionHandler = (_ session: Session, _ result: Result<____, MatrixError>) -> Void
+    
+    // MARK: Creating an Matrix Object
+    public let credentials = DirectionsCredentials
+    private let urlSession: URLSession
+    private let processingQueue: DispatchQueue
+    
+    /**
+     The shared matrix object.
+     
+     To use this object, a Mapbox [access token](https://docs.mapbox.com/help/glossary/access-token/) should be specified in the `MBXAccessToken` key in the main application bundle’s Info.plist.
+     */
+    public static let shared = Matrix()
+    
+    /**
+     Creates a new instance of Matrix object.
+     - Parameters:
+       - credentials: Credentials that will be used to make API requests to Mapbox Matrix API.
+       - urlSession: URLSession that will be used to submit API requests to Mapbox Matrix API.
+       - processingQueue: A DispatchQueue that will be used for CPU intensive work.
+     */
+    public init(credentials: DirectionsCredentials = .init(),
+                urlSession: URLSession = .shared,
+                processingQueue: DispatchQueue = .global(qos: .userInitiated)) {
+        self.credentials = credentials
+        self.urlSession = urlSession
+        self.processingQueue = processingQueue
+    }
+    
+    // MARK: Getting Matrix
+    // TODO: Calculate matrices here!
+    /**
+     Begins asynchronously calculating matrices using the given options and delivers the results to a closure.
+     
+     This method retrieves the matrices asynchronously from the [Mapbox Matrix API](https://docs.mapbox.com/api/navigation/matrix/) over a network connection. If a connection error or server error occurs, details about the error are passed into the given completion handler in lieu of the contours.
+     
+     - parameter options: A `MatrixOptions` object specifying the requirements for the resulting matrices.
+     - parameter completionHandler: The closure (block) to call with the resulting matrices. This closure is executed on the application’s main thread.
+     - returns: The data task used to perform the HTTP request. If, while waiting for the completion handler to execute, you no longer want the resulting matrices, cancel this task.
+     */
+    @discardableResult open func calculate(_ options: MatrixOptions, completionHandler: @escaping MatrixCompletionHandler) -> URLSessionDataTask {
+        let session = (options: options, credentials: self.credentials)
+        let request = urlRequest(forCalculating: options)
+        let requestTask = urlSession.dataTask(with: request) { (possibleData, possibleResponse, possibleError) in
+            
+            if let urlError = possibleError as? URLError {
+                DispatchQueue.main.async {
+                    completionHandler(session, .failure(.network(urlError)))
+                }
+                return
+            }
+            
+            guard let response = possibleResponse, ["application/json", "text/html"].contains(response.mimeType) else {
+                DispatchQueue.main.async {
+                    completionHandler(session, .failure(.invalidResponse(possibleResponse)))
+                }
+                return
+            }
+            
+            // TODO: CHECK THIS ERROR
+            guard let data = possibleData else {
+                DispatchQueue.main.async {
+                    completionHandler(session, .failure(.noData))
+                }
+            }
+            
+            self.processingQueue.async {
+                do {
+                    let decoder = JSONDecoder()
+                    
+                    // TODO: is this userinfo needed?
+                    decoder.userInfo = [.options: options,
+                                        .credentials: self.credentials]
+                    
+                    guard let disposition = try? decoder.decode(ResponseDisposition.self, from: data) else {
+                        let apiError = MatrixError(code: nil, message: nil, response: response, underlyingError: possibleError)
+                        
+                        DispatchQueue.main.async {
+                            completionHandler(session, .failure(apiError))
+                        }
+                        return
+                    }
+                    
+                    guard let (disposition.code == nil && disposition.message == nil) || disposition.code == "Ok" else {
+                        let apiError = MatrixError(code: disposition.code, message: disposition.message, response: response, underlyingError: possibleError)
+                        
+                        DispatchQueue.main.async {
+                            completionHandler(session, .failure(apiError))
+                        }
+                        return
+                    }
+                    
+                    // TODO: GET RETURN TYPE SORTED OUT AND UPDATE THIS
+                    let result = try decoder.decode(<#T##type: Decodable.Protocol##Decodable.Protocol#>, from: data)
+                    
+                    // TODO: Check for nil values?
+                    
+                    DispatchQueue.main.async {
+                        completionHandler(session, .success(result))
+                    }
+                    
+                } catch {
+                    DispatchQueue.main.async {
+                        let bailError = MatrixError(code: nil, message: nil, response: response, underlyingError: error)
+                        completionHandler(session, .failure(bailError))
+                    }
+                }
+            }
+        }
+        requestTask.priority = 1
+        requestTask.resume()
+        
+        return requestTask
+    }
+    
+    // MARK: Request URL Preparation
+    /**
+     The GET HTTP URL used to fetch the matrices from the Matrix API.
+     
+     - parameter options: A `MatrixOptions` object specifying the requirements for the resulting contours.
+     - returns: The URL to send the request to.
+     */
+    open func url(forCalculating options: MatrixOptions) -> URL {
+        
+        var params = options.urlQueryItems
+        params.append(URLQueryItem(name: "access_token", value: credentials.accessToken))
+
+        let unparameterizedURL = URL(string: options.path, relativeTo: credentials.host)!
+        var components = URLComponents(url: unparameterizedURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = params
+        return components.url!
+    }
+    
+    /**
+     The HTTP request used to fetch the matrices from the Matrix API.
+     
+     - parameter options: A `MatrixOptions` object specifying the requirements for the resulting routes.
+     - returns: A GET HTTP request to calculate the specified options.
+     */
+    open func urlRequest(forCalculating options: MatrixOptions) -> URLRequest {
+        let getURL = self.url(forCalculating: options)
+        var request = URLRequest(url: getURL)
+        request.setValue(userAgent, forHTTPHeaderField: "User-Agent")
+        return request
+    }
+}
--- Sources/MapboxDirections/MatrixOptions.swift
+++ Sources/MapboxDirections/MatrixOptions.swift
@@ -11,10 +11,10 @@ public class MatrixOptions: Codeable {
     /**
      Initializes a matrix options object for matrices and a given profile identifier.
      
-     - parameter coordinates: An array of `LocationCoordinate2D` objects representing locations that will be in the matrix. The array should contain at least X coordinates and at most X coordinates. (Some profiles, such as `DirectionsProfileIdentifier.automobileAvoidingTraffic`, [may have lower limits](https://docs.mapbox.com/api/navigation/matrix/#matrix-api-restrictions-and-limits).)
+     - parameter coordinates: An array of `Waypoints` objects representing locations that will be in the matrix. The array should contain at least X coordinates and at most X coordinates. (Some profiles, such as `DirectionsProfileIdentifier.automobileAvoidingTraffic`, [may have lower limits](https://docs.mapbox.com/api/navigation/matrix/#matrix-api-restrictions-and-limits).)
      - parameter profileIdentifier: A string specifying the primary mode of transportation for the routes.
      */
-    public init(coordinates: [LocationCoordinate2D], profileIdentifier: DirectionsProfileIdentifier) {
+    public init(coordinates: [Waypoints], profileIdentifier: DirectionsProfileIdentifier) {
         self.coordinates = coordinates
         self.profileIdentifier = profileIdentifier
     }
@@ -24,9 +24,13 @@ public class MatrixOptions: Codeable {
      */
     public enum SideOfRoad {
         /**
-         
+         The route can arrive at the waypoint from either side of the road.
          */
         case unrestricted
+        
+        /**
+         The route will arrive at the waypoint on the driving side of the region.
+         */
         case curb
     }
     
@@ -38,7 +42,7 @@ public class MatrixOptions: Codeable {
     /**
      An array of `LocationCoordinate2D` objects representing locations that will be in the matrix.
      */
-    public var coordinates: [LocationCoordinate2D]
+    public var coordinates: [Waypoints]
     
     /**
      AttributeOptions for the matrix. Only `AttributeOptions.distance` and `AttributeOptions.expectedTravelTime` can be specified.
@@ -49,15 +53,25 @@ public class MatrixOptions: Codeable {
     public var attributeOptions: AttributeOptions?
     
     /**
-     The side of the road from which to approach waypoints in the requested route.
+     The side of the road from which to approach waypoints in the requested route. The array must include a `SideOfRoad` for each coordinate.
+     
+     By default, `SideOfRoad.unrestricted` is specified for all waypoints.
      */
-    // TODO: Include default
-    // Shoul
     public var approachSides: [SideOfRoad]?
     
-    // TODO: How to handle index vs .all values? default to .all
-    public var destinations: [Int]? = []
-    public var sources: [Int]? = []
+    /**
+     The coordinates at a given index in the `coordinates` array that should be used as destinations.
+     
+     By default, all waypoints in the `coordinates` array are used as destinations.
+     */
+    public var destinations: [Int]?
+    
+    /**
+     The coordinates at a given index in the `coordinates` array that should be used as sources.
+     
+     By default, all waypoints in the `coordinates` array are used as sources.
+     */
+    public var sources: [Int]?
     
     
     private enum CodingKeys: String, CodingKey {
@@ -81,7 +95,7 @@ public class MatrixOptions: Codeable {
     
     public required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        coordinates = try container.decode([LocationCoordinate2D].self, forKey: .coordinates)
+        coordinates = try container.decode([Waypoints].self, forKey: .coordinates)
         profileIdentifier = try container.decode(ProfileIdentifier.self, forKey: .profileIdentifier)
         attributeOptions = try container.decodeIfPresent(AttributeOptions.self, forKey: .attributeOptions)
         approachSides = try container.decodeIfPresent([SideOfRoad].self, forKey: .approachSides)
@@ -109,6 +123,7 @@ public class MatrixOptions: Codeable {
     /**
      An array of URL query items (parameters) to include in an HTTP request.
      */
+    // TODO: Fix appended query items formatting!
     public var urlQueryItems: [URLQueryItem] {
         var queryItems: [URLQueryItem] = []
         
